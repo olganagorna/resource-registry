4bb4dda407b285758d8259a2f28f6f03d341075b
Edward Litvinchyck <Edward Litvinchyck>
2015-11-02 16:57:20 +0200

    database login information fix

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.9.112.2;dbname=resource_registry',
-    'username' => 'adminH3WPtev',
-    'password' => 'VqCknMszRKd6',
+    'dsn' => 'mysql:host=127.9.112.2;dbname=resource',
+    'username' => 'adminXvheCjv ',
+    'password' => 'tkhcjD_x4VX3',
     'charset' => 'utf8',
 ];
