a3eb8908df4a5541efc7bac59e0f79714cbde737
YuriyTeslenko <YuriyTeslenko>
2016-04-14 14:51:43 +0300

    community selection done

--- web/controllers/commissioner_controller.js
+++ web/controllers/commissioner_controller.js
@@ -16,15 +16,13 @@
                .then(successHandler)
                .catch(errorHandler);
            function successHandler(result) {
-               console.log(result.data);
                for (var i = 0; i < result.data.length; i++) {
-                 if (result.data[i].commissioner_id == commissCtrl.communId){
+                 if (result.data[i].community_id == commissCtrl.communId){
                   commissCtrl.name = result.data[i].name;
                   commissCtrl.prefix = result.data[i].prefix;
                   break;
                  }
                }
-               //commissCtrl.name = 'error';
            }
            function errorHandler(result){
                alert(result.data[0].message);
