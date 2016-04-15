497dfbd13fa7aef3918f8f381f502e755e23559f
genyklemberg <aputop@yahoo.com>
2016-04-15 13:24:51 +0300

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.2.238.2:3306;dbname=resre',
+    'username' => 'adminXTUqZHe',
+    'password' => '3M98vFIeVJrU',
+    'charset' => 'utf8',
+];
+
+// Jenkins
+// admin
+// Mtblp-ujF24-
