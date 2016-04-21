60de1fecf36aba141dafca5324d607509fd7adbe
Andrii Uhryn <a_ugryn@ukr.net>
2016-04-21 15:54:08 +0300

    a_uhryn merge (community page done)

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=resource_registry',
     'username' => 'root',
-    'password' => '7160722',
+    'password' => '1slovn',
     'charset' => 'utf8',
 ];
 
