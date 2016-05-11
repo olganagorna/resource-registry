8b7ab29448ffda70729b9c28a8666d2f6aefeee6
YuriyTeslenko <YuriyTeslenko>
2016-05-11 20:55:48 +0300

    fix PersonalSearch

--- controllers/Personal_dataController.php
+++ controllers/Personal_dataController.php
@@ -19,31 +19,32 @@ class Personal_dataController extends ActiveController
     }
     public function actionSearch()
     {
-        die();
-    	$GET = \Yii::$app->request->get();
-    	if (!empty($GET)) {
-    		$model = new $this->modelClass;
-    		foreach ($GET as $key => $value) {
-    			if (!$model->hasAttribute($key)) {
-    				throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
-    			}
-    		}
-    		try {
-    			$provider = new ActiveDataProvider([
-    					'query' => $model->find()->where($GET),
-    					'pagination' => false
-    			]);
-    		} catch (Exception $ex) {
-    			throw new \yii\web\HttpException(500, 'Internal server error');
-    		}
+        $GET = \Yii::$app->request->get();
+        if (!empty($GET)) {
+            $model = new $this->modelClass;
+            foreach ($GET as $key => $value) {
+                if (!$model->hasAttribute($key)) {
+                    throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
+                }
+            }
+            try {
+                $provider = new ActiveDataProvider([
+                        'query' => $model->find()->where($GET),
+                        'pagination' => false
+                ]);
+            } catch (Exception $ex) {
+                throw new \yii\web\HttpException(500, 'Internal server error');
+            }
 
-    		if ($provider->getCount() <= 0) {
-    			throw new \yii\web\HttpException(404, 'No entries found with this query string');
-    		} else {
-    			return $provider;
-    		}
-    	} else {
-    		throw new \yii\web\HttpException(400, 'There are no query string');
-    	}
+            if ($provider->getCount() <= 0) {
+                throw new \yii\web\HttpException(404, 'No entries found with this query string');
+            } else {
+                return $provider;
+            }
+        } else {
+            throw new \yii\web\HttpException(400, 'There are no query string');
+        }
     }
+
+    
 }
