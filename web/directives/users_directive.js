032ac8f5abbe586adf9621513581b6de9e914b50
YuriyTeslenko <YuriyTeslenko>
2016-05-12 15:33:53 +0300

    LVPHP-161
    "All user types shoud have their own access authority."
    
    Added angular http interceptor

--- web/directives/users_directive.js
+++ web/directives/users_directive.js
@@ -2,16 +2,21 @@
 
 	'use strict';
 
-	angular.module('restApp').directive("usersList", function () {
-
-		return {
-			restrict: 'E',
-			templateUrl: 'views/site/users.html',
-			controller: 'UsersController'
-			
-		};
-
-	});
+	angular.module('restApp').directive('errorBox', function (AUTH_EVENTS) {
+  return {
+    restrict: 'E',
+    template: '<div ng-if="visible" ng-include="\'views/site/errorbox.html\'">',
+    link: function (scope) {
+      var showDialog = function () {
+        scope.visible = true;
+      };
+  
+      scope.visible = false;
+      scope.$on(AUTH_EVENTS.notAuthorized, showDialog);
+      scope.$on(AUTH_EVENTS.sessionTimeout, showDialog)
+    }
+  };
+})
 
 
 })();
--- web/services/auth_service.js
+++ web/services/auth_service.js
@@ -5,15 +5,13 @@
         .module('restApp')
         .factory('AuthInterceptor', AuthInterceptor);
 
-    AuthInterceptor.$inject = ['$rootScope', '$q'];
-    function AuthInterceptor($rootScope, $q) {
+    AuthInterceptor.$inject = ['$rootScope', '$q', 'AUTH_EVENTS'];
+    function AuthInterceptor($rootScope, $q, AUTH_EVENTS) {
         return {
             responseError: function (response) { 
               $rootScope.$broadcast({
                 401: AUTH_EVENTS.notAuthenticated,
-                403: AUTH_EVENTS.notAuthorized,
-                419: AUTH_EVENTS.sessionTimeout,
-                440: AUTH_EVENTS.sessionTimeout
+                403: AUTH_EVENTS.notAuthorized
               }[response.status], response);
               return $q.reject(response);
             }
