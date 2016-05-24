60e2315380499803ef5fc7ef0adf6e850fbaf972
YuriyTeslenko <YuriyTeslenko>
2016-05-24 16:39:03 +0300

    cleaup

--- web/modules/app_module.js
+++ web/modules/app_module.js
@@ -112,5 +112,21 @@
             var test = localStorage.getItem('username');
         });
     }
-
+///
+/*.run(function ($rootScope, AUTH_EVENTS, AuthService) {
+  $rootScope.$on('$stateChangeStart', function (event, next) {
+    var authorizedRoles = next.data.authorizedRoles;
+    if (!AuthService.isAuthorized(authorizedRoles)) {
+      event.preventDefault();
+      if (AuthService.isAuthenticated()) {
+        // user is not allowed
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      } else {
+        // user is not logged in
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+      }
+    }
+  });
+})*/
+///
 })();
