64e1fcc8b4b2c878bf25c29e461a53dbed659fa2
olganagorna <olechka.nagorna@gmail.com>
2016-05-27 19:54:30 +0300

    Lv-193 clear version with changes

--- controllers/Attribute_class_viewController.php
+++ controllers/Attribute_class_viewController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace app\controllers;
-
 use app\controllers\AppController;
 use app\models\ResourceAttribute;
 use app\models\AttributeClassView;
-
 class Attribute_class_viewController extends AppController
 {
     public $modelClass = 'app\models\AttributeClassView';
@@ -23,74 +20,32 @@ class Attribute_class_viewController extends AppController
     }
     public function actionSearch()
     {
-    	$GET = \Yii::$app->request->get();
-    	if (!empty($GET)) {
-    		$model = new $this->modelClass;
-    		foreach ($GET as $key => $value) {
-    			if (!$model->hasAttribute($key)) {
-    				throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
-    			}
-    		}
-    		try {
-    			$provider = new ActiveDataProvider([
-    					'query' => $model->find()->where($GET),
-    					'pagination' => false
-    			]);
-    		} catch (Exception $ex) {
-    			throw new \yii\web\HttpException(500, 'Internal server error');
-    		}
-    
-    		if ($provider->getCount() <= 0) {
-    			throw new \yii\web\HttpException(404, 'No entries found with this query string');
-    		} else {
-    			return $provider;
-    		}
-    	} else {
-    		throw new \yii\web\HttpException(400, 'There are no query string');
-    	}
-    }
-
-    public function actionAddattribute()
-    {
-        $last_id = ResourceAttribute::find()->select(['attribute_id'])->orderBy(['attribute_id' => SORT_DESC])->one();
-        $attribute_id = $last_id->attribute_id + 1;
-        if (!$post = \Yii::$app->getRequest()->getBodyParams()) {
-            throw new \yii\web\HttpException(400, 'Дані не отримані');
-        }
-        $resourceAttributeModel = new resourceAttribute();
-        if ($resourceAttributeModel->findByAttributeName($post['attribute_name'])){
-            throw new \yii\web\HttpException(400, 'Такий атрибут уже існує');
-        }
-        $attribute_name = $post['attribute_name'];
-        $resourceAttributeModel->name = $attribute_name;
-        $resourceAttributeModel->is_global = 0;
-        if (!$resourceAttributeModel->save()){
-            foreach($resourceAttributeModel->errors as $key){
-                $errorMessage .= $key[0];
+        $GET = \Yii::$app->request->get();
+        if (!empty($GET)) {
+            $model = new $this->modelClass;
+            foreach ($GET as $key => $value) {
+                if (!$model->hasAttribute($key)) {
+                    throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
+                }
             }
-            throw new \yii\web\HttpException(422,$errorMessage);
-        }
-        $attributeclassviewModel = new AttributeClassView();
-        $class_id = $post['class_id'];
-        $attributeclassviewModel->class_id = $class_id;
-        $attributeclassviewModel->attribute_id = $attribute_id;
-        if (!$attributeclassviewModel->save()){
-            foreach($attributeclassviewModel->errors as $key){
-                $errorMessage .= $key[0];
+            try {
+                $provider = new ActiveDataProvider([
+                        'query' => $model->find()->where($GET),
+                        'pagination' => false
+                ]);
+            } catch (Exception $ex) {
+                throw new \yii\web\HttpException(500, 'Internal server error');
             }
-            throw new \yii\web\HttpException(422,$errorMessage);
-        }
-    }
     
-    public function actionDeleteattribute()
-    {
-        $request = \Yii::$app->request->get();
-        $getdata = AttributeClassView::find()->where(['attribute_id'=>$request['attr_id']])->one();
-        if($getdata){
-            $getdata->delete();
+            if ($provider->getCount() <= 0) {
+                throw new \yii\web\HttpException(404, 'No entries found with this query string');
+            } else {
+                return $provider;
+            }
+        } else {
+            throw new \yii\web\HttpException(400, 'There are no query string');
         }
     }
-
     public function actionAttribute() 
     {
         $request = \Yii::$app->request->get();
--- controllers/Resource_classController.php
+++ controllers/Resource_classController.php
@@ -1,19 +1,16 @@
 <?php
 namespace app\controllers;
-
 use app\controllers\AppController;
 use yii\data\ActiveDataProvider;
 use app\models\ResourceClass;
 use app\models\AttributeClassView;
 use app\models\ResourceAttribute;
-
 class Resource_classController extends AppController
 {
     public $modelClass = 'app\models\ResourceClass';
     public $attrClassView = 'app\models\AttributeClassView';
     public $resAttribute = 'app\models\ResourceAttribute';
     
-
     public function behaviors()
     {
         return 
@@ -25,31 +22,31 @@ class Resource_classController extends AppController
     }
     public function actionSearch()
     {
-    	$GET = \Yii::$app->request->get();
-    	if (!empty($GET)) {
-    		$model = new $this->modelClass;
-    		foreach ($GET as $key => $value) {
-    			if (!$model->hasAttribute($key)) {
-    				throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
-    			}
-    		}
-    		try {
-    			$provider = new ActiveDataProvider([
-    					'query' => $model->find()->where($GET),
-    					'pagination' => false
-    			]);
-    		} catch (Exception $ex) {
-    			throw new \yii\web\HttpException(500, 'Internal server error');
-    		}
+        $GET = \Yii::$app->request->get();
+        if (!empty($GET)) {
+            $model = new $this->modelClass;
+            foreach ($GET as $key => $value) {
+                if (!$model->hasAttribute($key)) {
+                    throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
+                }
+            }
+            try {
+                $provider = new ActiveDataProvider([
+                        'query' => $model->find()->where($GET),
+                        'pagination' => false
+                ]);
+            } catch (Exception $ex) {
+                throw new \yii\web\HttpException(500, 'Internal server error');
+            }
     
-    		if ($provider->getCount() <= 0) {
-    			throw new \yii\web\HttpException(404, 'No entries found with this query string');
-    		} else {
-    			return $provider;
-    		}
-    	} else {
-    		throw new \yii\web\HttpException(400, 'There are no query string');
-    	}
+            if ($provider->getCount() <= 0) {
+                throw new \yii\web\HttpException(404, 'No entries found with this query string');
+            } else {
+                return $provider;
+            }
+        } else {
+            throw new \yii\web\HttpException(400, 'There are no query string');
+        }
     }
     public function actionChangeactivationstatus() 
     {
