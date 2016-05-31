ac2ebcf4014d1d1f8daafae05141b7593c3fa5b1
YuriyTeslenko <YuriyTeslenko>
2016-05-31 13:59:05 +0300

    Add user

--- web/controllers/registration_controller.js
+++ web/controllers/registration_controller.js
@@ -22,13 +22,15 @@
             community: ''
         };
 
+        vm.community = {community_id: 0};
+
         (function(){
             return $http.get('rest.php/communities')
 
                 .then(successHandler)
                 .catch(errorHandler);
             function successHandler(data) {
-                vm.communities = data.data;
+                vm.itemsList = data.data.items;
             }
             function errorHandler(data){
                 console.log("Can't render list!");
@@ -36,18 +38,8 @@
         }());
 
         vm.registration = function(){
-            console.log('test');
 
-            vm.user.username = vm.username;
-            vm.user.password = vm.password;
-            vm.user.email = vm.email;
-            vm.user.last_name = vm.last_name;
-            vm.user.first_name =  vm.first_name;
-            vm.user.middle_name = vm.middle_name;
-            vm.user.passport_series =  vm.passport_series;
-            vm.user.passport_number = vm.passport_number;
-            vm.user.address = vm.address;
-            vm.user.community = vm.community;
+            vm.user.community = vm.community.community_id;
 
             vm.send = function () {
                 return $http.post('rest.php/users/adduser', vm.user)
@@ -65,12 +57,9 @@
                     console.log(result);
                 }
             };
-            vm.send();
+            //vm.send();
+            console.log(vm.user);
         };
-
-
-
     }
-
 })();
 
--- web/directives/dropdown_directive.js
+++ web/directives/dropdown_directive.js
@@ -7,32 +7,30 @@
             restrict: 'E',
             scope: {
                 itemsList: '=',
-                placeholder: '@'
+                searchResult: '=',
+                placeholder: '@',
             },
-            template: '<input type="text" ng-model="search" placeholder="{{ placeholder }}" />' +
-        '<div class="search-item-list"><ul class="list">' +
-        '<li ng-repeat="item in itemsList | filter:search" ng-click="chooseItem( item )">{{ item.name }}' +
-          '<span class="amount">{{ item.amount }}</span>' +
-         '</li>' +
-        '</ul></div>',
+            template: '<input class="form-control" type="text" ng-model="search" placeholder="{{ placeholder }}" />' +
+                '<div class="search-item-list"><ul class="list-group material-list-group list">' +
+                    '<li class="list-group-item" ng-repeat="item in itemsList | toArray : false | filter:{isactive:1} | filter:search" ng-click="chooseItem( item )">{{ item.name }}' +
+                        '<span class="badge">{{ item.prefix }}</span>' +
+                    '</li>' +
+                '</ul></div>',
+
             link: function(scope, el, attr){
-        var $listContainer = angular.element( el[0].querySelectorAll('.search-item-list')[0] );
-        el.find('input').bind('focus',function(){
-          $listContainer.addClass('show');
-        });
-        el.find('input').bind('blur',function(){
-          /*
-             * 'blur' реагирует быстрее чем ng-click,
-             * поэтому без $timeout chooseItem не успеет поймать item до того, как лист исчезнет
-             */
-          $timeout(function(){ $listContainer.removeClass('show') }, 200);
-        });
-      
-        scope.chooseItem = function( item ){
-         scope.search = item.name;
-           $listContainer.removeClass('show');
-       }
-    }
+                var $listContainer = angular.element( el[0].querySelectorAll('.search-item-list')[0] );
+                el.find('input').bind('focus',function(){
+                    $listContainer.addClass('show');
+                });
+                el.find('input').bind('blur',function(){
+                    $timeout(function(){ $listContainer.removeClass('show') }, 200);
+                });
+                scope.chooseItem = function( item ){
+                    scope.search = item.name;
+                    scope.searchResult =  item;
+                    $listContainer.removeClass('show');
+                }
+            }
         };
     })
 })();
