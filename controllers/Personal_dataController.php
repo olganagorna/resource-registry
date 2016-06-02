a096940391c59e39bede6a89f5d516fd66aa891d
Andrii Uhryn <a_ugryn@ukr.net>
2016-06-02 13:47:48 +0300

    clean with base

--- controllers/Personal_dataController.php
+++ controllers/Personal_dataController.php
@@ -18,34 +18,6 @@ class Personal_dataController extends ActiveController
             ],
         ]);
     }
-    // public function actionSearch()
-    // {
-    //     $GET = \Yii::$app->request->get();
-    //     if (!empty($GET)) {
-    //         $model = new $this->modelClass;
-    //         foreach ($GET as $key => $value) {
-    //             if (!$model->hasAttribute($key)) {
-    //                 throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
-    //             }
-    //         }
-    //         try {
-    //             $provider = new ActiveDataProvider([
-    //                     'query' => $model->find()->where($GET),
-    //                     'pagination' => false
-    //             ]);
-    //         } catch (Exception $ex) {
-    //             throw new \yii\web\HttpException(500, 'Internal server error');
-    //         }
-
-    //         if ($provider->getCount() <= 0) {
-    //             throw new \yii\web\HttpException(404, 'No entries found with this query string');
-    //         } else {
-    //             return $provider;
-    //         }
-    //     } else {
-    //         throw new \yii\web\HttpException(400, 'There are no query string');
-    //     }
-    // }
     public function actionSearch(){
         $request= \Yii::$app->request->get();
         $param = [':passport_series' => $request['passport_series']];
