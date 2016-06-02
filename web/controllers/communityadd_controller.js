486d42fb45243c7efd9adf1aebbde0b3adfd8e3a
Y.Potupa <y.potupa@gmail.com>
2016-06-02 17:15:44 +0300

    U182 Req. & extr. full path development (separate views and filters for users and registrars) finall

--- web/controllers/communityadd_controller.js
+++ web/controllers/communityadd_controller.js
@@ -1,53 +1,35 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('restApp')
-    .controller('communityAddCtrl', communityAddCtrl);
+	angular
+		.module('restApp')
+    	.controller('communityAddCtrl', communityAddCtrl);
 
-  communityAddCtrl.$inject = ['$scope', '$http', '$routeParams'];
-  function communityAddCtrl($scope, $http, $routeParams) {
-    var  comAdd = this;
+    communityAddCtrl.$inject = ['$scope', '$http', '$routeParams'];
+  	function communityAddCtrl($scope, $http, $routeParams) {
+  		var  comAdd = this;
 
-    // comAdd.getUserFromDb = function() {
-    //   // search user in database
-    //   var data = {
-    //   username: comAdd.username,
-    //   };
-    //   var post = $http.post('rest.php/users/getuser', JSON.stringify(data))
-    //     .then(successHandler)
-    //     .catch(errorHandler);
-    //   function successHandler(result) {
-    //     comAdd.findUser = 'користувач "'+result.data.username+'" існує';
-    //     comAdd.commissioner_id = result.data.id;
-    //   }
-    //   function errorHandler(result){
-    //     console.log("Error"+result);
-    //     comAdd.findUser = 'користувач "'+comAdd.username+'" відсутній у базі';
-    //   }
-    // };    
-
-    comAdd.addCommunity = function() {
-      var dataMain = {
-        com_name: comAdd.community.name,
-        com_num: comAdd.community.num,
-        com_adds: comAdd.community.additions
-      };
-
-      (function() {
-        var post = $http.post('rest.php/communities/addcomm', JSON.stringify(dataMain))
-          .then(successHandler)
-          .catch(errorHandler);
-        function successHandler(result) {
-          $('.alert-success').toggle(); // show success alert
-          comAdd.community = null;
-          $scope.$setPristine(true);
-        }
-        function errorHandler(result){
-        }
-      })();
-      
-    }
-  }
-
+	  	comAdd.addCommunity = function() {
+	  		var dataMain = {
+		        com_name: comAdd.community.name,
+		        com_num: comAdd.community.num,
+		        com_adds: comAdd.community.additions
+	    	};
+	      	(function() {
+		        var post = $http.post('rest.php/communities/addcomm', JSON.stringify(dataMain))
+		        	.then(successHandler)
+		        	.catch(errorHandler);
+		        function successHandler(result) {
+		        	$('.alert-success').toggle(); // show success alert
+		        	comAdd.community = null;
+		        	$scope.$setPristine(true);
+		        }
+		        function errorHandler(result){
+		        	if(result.data.message.includes('13 characters')){
+                        alert('введіть у форматі 123:45:67:890');
+                    } else alert(result.data.message);
+	        	}
+	    	})();
+	    }
+  	}
 })();
